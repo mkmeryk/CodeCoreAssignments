2e75c6f172dcb5b61d03965effd8cf1096a0036f
mkmeryk <inq@meryk.ca>
2022-02-03 22:02:56 -0800

    box it function

--- box_it_script/box_it_script.js
+++ box_it_script/box_it_script.js
@@ -1,12 +1,19 @@
-//draw line function that takes a number as an argument and returns 
-//the number of horizental bars as a string
+//calculating the max length by comparing all the elements in the given array of names
+const maxLength = function(arr){
+    let tempLength = 0
+    for( let i = 0; i<arr.length ; i++){
+        if(arr[i].length>tempLength){
+            tempLength = arr[i].length
+        }
+    }
+    return tempLength
+}
 
-// const { connect } = require("http2")
-var line = ''
 
+//a function that takes a number and returns 
+//the number of horizantal lines to draw a line 
 const drawLine = function(a){
-    // var line = ''
-    
+    let line = ''
     for(let i = a; i > 0; i--){
         let bar = '\u2550'
         line = line + bar
@@ -15,37 +22,71 @@ const drawLine = function(a){
     return line
 }
 
-// draw top border, draw middle border and draw bottom border function
-//each function should take a number and return a line of length including including 
-//corner pieces
+//by implementing the drawLine function creating three new function that are 
+//diffrent depenig on their position cpmaring to elements of the given array
 
 //top border
 function drawTopBorder(a){
     
-    return '\u2554' + line + '\u2557'
+    return '\u2554' + drawLine(a) + '\u2557'
 }
 
 //midlle border
 function drawMiddleBorder(a){
 
-    return '\u2560' + line + '\u2563'
+    return '\u2560' + drawLine(a)  + '\u2563'
 }
 
 //bottom border
 function drawBottomBorder(a){
    
-    return '\u255a' + line + '\u255d'
+    return '\u255a' + drawLine(a)  + '\u255d'
 }
 
-//draw barss around function which takes a string and surrounds it
-//with verstical lines
-
-function drawBarsAround(str){
-    return '\u2551' + str + '\u2551'
+//draw barss around function which takes a string and surrounds itwith verstical lines
+function drawBarsAround(str,arr){
+    //since not all the elements have the same length, some of the shorter names require
+    //more space after the name so that the side bars would be in line with the rest of lines
+    let extraSpace = ''
+    //checking if the given string is shorter than the max length to add more space to
+    //the end of the name
+    if(str.length < maxLength(arr)){
+        const countSpace = maxLength(arr) - str.length
+        for(i = countSpace; i > 0 ; i--)
+        extraSpace = extraSpace + ' '
+    }
+    return '\u2551' + str + extraSpace + '\u2551'
 }
 
-drawLine(7)
-console.log(drawTopBorder(5))
-console.log(drawMiddleBorder(5))
-console.log(drawBottomBorder(5))
-console.log(drawBarsAround("My name is Dan"))
+//box it function that takes an array of string and returns a string in seprate lines
+function boxIt(arr){
+    //empty array as a container to box all before returning them as a string where each 
+    //is in single column tables. using .join("\n")
+    const tempArray = []
+
+    //depending on what position in the array the string has in order to add diffrent parts of the box
+    for(let i = 0; i <= arr.length; i++){
+        if(i === 0){
+            //if the string is the first position within the array, it start with adding the top border to
+            //tempArray then takes the string addes bars around it and adds it the tempArray
+            tempArray.push(drawTopBorder(maxLength(arr)))
+            tempArray.push(drawBarsAround(arr[0],arr))
+        }else if( i < arr.length ){
+            //if it is any other postion in the array it draws the middle bars that would go between
+            //two names and then takes the string and adds a border around it
+            tempArray.push(drawMiddleBorder(maxLength(arr)))
+            tempArray.push(drawBarsAround(arr[i],arr))
+        }else{
+            //if it is more than the length of the array meaning there are no more names to execute
+            //it would draw the bottom line
+            tempArray.push(drawBottomBorder(maxLength(arr)))
+        }
+    }
+
+    //takes the tempArray and truns it into a string and would add new lines in betwwen each element of the array
+    const boxIt = tempArray.join('\n')
+    return boxIt
+}
+
+
+console.log(boxIt(['maybe','no','hello','hi','sammuel']))

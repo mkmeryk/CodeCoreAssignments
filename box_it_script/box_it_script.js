7179e534d3e625949e7756f4ba70f2ce0b01e3af
mkmeryk <inq@meryk.ca>
2022-02-05 01:07:03 -0800

    added some comments

--- box_it_script/box_it_script.js
+++ box_it_script/box_it_script.js
@@ -1,8 +1,11 @@
 #!/home/mk/.nvm/versions/node/v17.4.0/bin/node
-//attempting the stretch - 1
+//assignment stretch part 1
 //to access the program throgh cli with out using command node
 //add #!/path to node and run this command in cli
 //chmod +x box_it_script.js
+
+//assignment stretch part 2
+//attempted but was not able to seprate the original code from the strech part code
 
 //turning the file into a screipt to use from cli
 //an empty array to push the arguments taken from the cli
@@ -11,7 +14,7 @@ let arr = []
 //taking n numbers of the givven areguments from the cli to add to the empty array
 for (let j = 2; j < process.argv.length; j++) {
     arr.push(process.argv[j]);
-}
+};
 
 
 
@@ -21,23 +24,23 @@ const maxLength = function(arr){
     for( let i = 0; i<arr.length ; i++){
         if(arr[i].length>tempLength){
             tempLength = arr[i].length
-        }
-    }
-    return tempLength
-}
+        };
+    };
+    return tempLength;
+};
 
 
 //a function that takes a number and returns 
 //the number of horizantal lines to draw a line 
 const drawLine = function(a){
-    let line = ''
+    let line = '';
     for(let i = a; i > 0; i--){
         let bar = '\u2550'
         line = line + bar
-    }
+    };
 
-    return line
-}
+    return line;
+};
 
 //by implementing the drawLine function creating three new function that are 
 //diffrent depenig on their position cpmaring to elements of the given array
@@ -45,26 +48,26 @@ const drawLine = function(a){
 //top border
 function drawTopBorder(a){
     
-    return '\u2554' + drawLine(a) + '\u2557'
-}
+    return '\u2554' + drawLine(a) + '\u2557';
+};
 
 //midlle border
 function drawMiddleBorder(a){
 
-    return '\u2560' + drawLine(a)  + '\u2563'
-}
+    return '\u2560' + drawLine(a)  + '\u2563';
+};
 
 //bottom border
 function drawBottomBorder(a){
    
-    return '\u255a' + drawLine(a)  + '\u255d'
-}
+    return '\u255a' + drawLine(a)  + '\u255d';
+};
 
 //draw barss around function which takes a string and surrounds itwith verstical lines
 function drawBarsAround(str,arr){
     //since not all the elements have the same length, some of the shorter names require
     //more space after the name so that the side bars would be in line with the rest of lines
-    let extraSpace = ''
+    let extraSpace = '';
 
     //checking if the given string is shorter than the max length to add more space to
     //the end of the name
@@ -72,16 +75,16 @@ function drawBarsAround(str,arr){
         const countSpace = maxLength(arr) - str.length
         for(i = countSpace; i > 0 ; i--)
         extraSpace = extraSpace + ' '
-    }
+    };
 
-    return '\u2551' + str + extraSpace + '\u2551'
-}
+    return '\u2551' + str + extraSpace + '\u2551';
+};
 
 //box it function that takes an array of string and returns a string in seprate lines
 function boxIt(arr){
     //setting an empty array as a container to box all before returning them as a string where each 
     //is in single column tables. using .join("\n")
-    const tempArray = []
+    const tempArray = [];
 
     //checking if the given array is empty to print an empty box
     if(arr.length === 0){
@@ -105,15 +108,15 @@ function boxIt(arr){
                 //if it is more than the length of the array meaning there are no more names to execute
                 //it would draw the bottom line
                 tempArray.push(drawBottomBorder(maxLength(arr)))
-            }
-        }
-    }
+            };
+        };
+    };
     
 
     //takes the tempArray and truns it into a string and would add new lines in betwwen each element of the array
-    const boxIt = tempArray.join('\n')
-    return boxIt
-}
+    const boxIt = tempArray.join('\n');
+    return boxIt;
+};
 
 //calling the boxIt function with the arugments given from the cli
-console.log(boxIt(arr))
+console.log(boxIt(arr));

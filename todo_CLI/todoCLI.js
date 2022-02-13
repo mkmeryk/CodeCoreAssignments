6be784270d4eda8f0726cb1936fe08934e7b48b6
mkmeryk <inq@meryk.ca>
2022-02-13 13:48:26 -0800

    the menu

---
+++ todo_CLI/todoCLI.js
@@ -0,0 +1,43 @@
+/*The Menu
+When the todoCLI.js is first executed, a menu as shown below should be displayed. 
+These are all the options the user should be able to perform.*/
+
+function Menu(){
+    console.log(`\nWelcome to Todo CLI! \n------------------- \n(v)View \u2022 (n)New \u2022 (cX)Complete \u2022 (dX)Delete \u2022 (q)Quit \n`);
+};
+
+
+/* View
+From the Todo Menu, pressing v then Enter should display the contents
+of the todo list then the Todo Menu again. See the example below.*/
+
+
+
+/*Add
+From the Todo Menu pressing n then Enter should ask the user what 
+item to add to the list. The user can then write a response. Save their response as 
+a new item at the end of the todo list. */
+
+
+
+/* Complete
+From the Todo Menu pressing cX where X refers to the index 
+of a Todo item then Enter should mark that item as complete. 
+Tell the user which item was marked. Then, re-display the Todo Menu.*/
+
+
+
+/*Delete
+From the Todo Menu pressing dX where X refers to the index 
+of a Todo item then `Enter` should remove that from the list. 
+Tell the user which item was deleted. Then, re-display the Todo Menu. */
+
+
+
+/*Quit
+From the Todo Menu pressing q quits the application. Say farewell. */
+/* */
+
+
+
+Menu();

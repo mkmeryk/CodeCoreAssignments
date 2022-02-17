208fbed2757735f99da222267139030663de553e
mkmeryk <inq@meryk.ca>
2022-02-16 22:51:43 -0800

    added some comments

--- todo_CLI/todoCLI.js
+++ todo_CLI/todoCLI.js
@@ -7,15 +7,17 @@ const interface = readline.createInterface(
     }
 );
 
+//an empty array for to do items
 let list = []
 
 interface.prompt();
 interface.on('line', line => {
-    if(line == 'v'){
+    
+    if(line == 'v'){    //View function to print all the tasks
         
         view();
 
-    }else if ( line == 'n'){
+    }else if ( line == 'n'){    //New function to add a new task which has a task name and a bloolean value to calculate if the task has been completed or not
     
         console.log("What?")
         interface.question("", (newTask) =>{
@@ -23,21 +25,21 @@ interface.on('line', line => {
         });
     
 
-    }else if(line[0] == 'c'){
+    }else if(line[0] == 'c'){    //Compelete function in which a the input is followed by a number that identifies which task is supposed to be completed and would return the completed task to the user
 
         completed(line[1]);
 
-    }else if(line[0] == 'd'){
+    }else if(line[0] == 'd'){    //Delete function in which a the input is followed by a number that identifies which task is supposed to be deleted and would return the deleted task to the user
 
         del(line[1]);
 
-    }else if( line == 'q'){
+    }else if( line == 'q'){    //Quit function which print a good bye message and quit the program
 
         quit();
 
     }else{
 
-        console.log("undefined task")
+        console.log("undefined task")    //In the event of user inputing an un identified input/task it would show an error message
     }
 
 })
@@ -61,18 +63,18 @@ of the todo list then the Todo Menu again. See the example below.*/
 
 function view(){
 
-    if(list.length == 0){
+    if(list.length == 0){    //If there is no tasks in the the todo list it would print a message and would ask for more input
 
         console.log("List is empty...");
     
     }else{
 
-        for( let i = 0; i< list.length; i++){
+        for( let i = 0; i< list.length; i++){    //For each item in the list it would check to see if it has been completed or not and would print it diffrently depending on if the item has a value of ture or false
 
             let numberOfTask = i;
-            if(list[i].completed == false){
-                console.log( numberOfTask + "-[  ] " + list[i].newTask);
-            }else{
+            if(list[i].completed == false){    //If the items complete value is false would show it as a task waiting to be compeleted
+                console.log( numberOfTask + "-[  ] " + list[i].newTask);    
+            }else{    //if the items complete value is trueit would show the item with a check mark to indicate that the item has been completed
                 console.log( numberOfTask + "-[✓ ] " + list[i].newTask);
             }
             
@@ -91,7 +93,7 @@ a new item at the end of the todo list. */
 
 function add(newTask){
 
-    list.push(newTask);
+    list.push(newTask);    //would add the new give task with the defult completed value of false to the list
     menu();
 
 }
@@ -103,14 +105,13 @@ Tell the user which item was marked. Then, re-display the Todo Menu.*/
 
 function completed(n){
 
-    if(n>list.length || n < 0 || isNaN(n) == true || list.length == 0){
+    if(n>list.length || n < 0 || isNaN(n) == true || list.length == 0){    //checks for invalid inputs
         console.log("Invalid task number, please refer to the number printed by each task")
     }else{
-        (list[n]).completed = true;
+        (list[n]).completed = true;    //changes the completed value of the item with the index of n to true 
         console.log(`Completed task: ${list[n].newTask}`);
     }
     
-
 }
 
 /*Delete
@@ -121,12 +122,11 @@ Tell the user which item was deleted. Then, re-display the Todo Menu. */
 //since the delete frase is a js reserved frase del stands for delete
 function del(n){
 
-    //if the given number is not an actual existing task
-    if(n>list.length || n < 0 || isNaN(n) == true || list.length == 0){
+    if(n>list.length || n < 0 || isNaN(n) == true || list.length == 0){    //checks for invalid inputs
         console.log("Invalid task number, please refer to the number printed by each task")
     }else{
         console.log(`Deleted task: ${list[n].newTask}`);
-        list.splice(n);
+        list.splice(n);    //removes the item with the index of n
     }
   
 }
@@ -135,10 +135,12 @@ function del(n){
 /*Quit
 From the Todo Menu pressing q quits the application. Say farewell. */
 /* */
+
+
 function quit(){
 
     console.log("See you soon!");
-    process.exit();
+    process.exit();    //stops asking for user inputs
 
 }
 

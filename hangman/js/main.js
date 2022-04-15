c74b53e0208b65b3e5c356267fa358c784693234
mkmeryk <inq@meryk.ca>
2022-04-15 04:55:39 -0700

    added logic to pick a random word on a new round

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -1,23 +1,26 @@
 $(document).ready(function(){
 
-    // const keywords = ["greyling","greydwarf","brute","shaman","troll","ghost","abomination"];
-    // const keyword = keywords[Math.floor(Math.random() * (keywords.length))];
-    // console.log(keyword.length);
-    // let correctGuessed = [];
-    // for(let i=0 ; i <= keyword.length ; i++){
-    //     correctGuessed += '_ '
-    // }
-    // console.log(correctGuessed);
+    //an array of words to be randomly selected for each round
+    const keywords = ["greyling","greydwarf","brute","shaman","troll","ghost","abomination"];
+    const keyword = keywords[Math.floor(Math.random() * (keywords.length))];
 
+    //depending on the random selected word there will be blank spaces to be filled later
+    let correctGuessed = [];
+    for(let i=0 ; i <= (keyword.length)-1 ; i++){
+        correctGuessed.push('_ ');
+    }
+
+    //how many guesses per game a user has
     let hearts = 6;
     let lettersClicked = "";
-    let correctGuessed = ['_ ','_ ','_ ','_ ','_ ','_ '];
-    const keyword = "action";
+
+    //the letters on the keyboard
     const letters = document.querySelectorAll(".keyboard-letters");
+
+    //audio file
     const failureSound = new Audio("staticAssets/sounds/failure.wav");
     failureSound.volume = 0.4
 
-    
     const img0 = "./staticAssets/img/gallows+head.jpg";
     const img1 = "./staticAssets/img/gallows+head+torso.jpg";
     const img2 = "./staticAssets/img/gallows+head+torso+leg.jpg";
@@ -25,10 +28,15 @@ $(document).ready(function(){
     const img4 = "./staticAssets/img/gallows+head+torso+2leg+arm.jpg";
     const img5 = "./staticAssets/img/gallows+head+torso+2leg+2arm.jpg";
 
+    //array of images of different state of the hangman
     const images = [img5,img4,img3,img2,img1,img0];
 
     letters.forEach(node =>{
         node.addEventListener('click', event=>{
+
+            //removing the getting started tag
+            $('.msg').remove();
+
             //changing the color of a clicked letter
             event.target.classList.add("clicked");
 
@@ -43,31 +51,35 @@ $(document).ready(function(){
 
             //when a letter is clicked it can not be clicked again as it gets disabled 
             $(event.target).attr("disabled","true");
-            
+
             //checking to see if the clicked letter is in the phrase
             if (keyword.includes(clickedLetter)) {
 
                 //adding only the correct guessed letters
-                // correctGuessed += clickedLetter
+               
                 
                 //phrase action has 6 letters # # # # # # => if a is entered it should replace the first child
                 //if n is entered it should be replaced with the last child or the 6th child
 
                 //checking the index number of the guessed letter that exist in the phrase
-                let correctGuessedIndex = keyword.indexOf(clickedLetter);
-                correctGuessed.forEach(element => {
-                    correctGuessed[correctGuessedIndex] = clickedLetter
-                });
+                let currentCorrectGuessedIndex = keyword.indexOf(clickedLetter);
+                while (currentCorrectGuessedIndex !== -1 && currentCorrectGuessedIndex < keyword.length) {
+                    //correctGuessedIndices.push(currentCorrectGuessedIndex);
+                    correctGuessed[currentCorrectGuessedIndex] = clickedLetter;
+                    currentCorrectGuessedIndex = keyword.indexOf(clickedLetter,currentCorrectGuessedIndex+1)
+                }
 
                 //replacing the empty line with the correct guessed letter in an appropriate index 
-                $('h1').html(`${correctGuessed.join('')}`)
+                $('h1').html(`${correctGuessed.join('')}`);
                 
-                //finishing the game
+                //finishing the game as a winner
                 if((correctGuessed.includes('_ ')) !== true){
-                    alert("Congrats you have won! Click okay to play another round");
-                    location.reload();
+                    setTimeout(() => {
+                        alert("Congrats you have won! Click okay to play another round"),
+                        location.reload()
+                    }, 500)
                 }
-                
+            
             } else {
 
                 //lower the hearts
@@ -76,7 +88,7 @@ $(document).ready(function(){
                 //change the images accordingly to the amount of wronged guesses
                 $("img").attr("src",images[hearts])
                 
-
+                //finishing the game as a lost round
                 if(hearts <= 0){
                     failureSound.play();
                     alert("Game over try again, click ok to refresh the page");
@@ -87,6 +99,7 @@ $(document).ready(function(){
             }
         })
     })    
+
 })
 
 

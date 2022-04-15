ac844f0cb17c5679046b66f26ade9b7bb7a45291
mkmeryk <inq@meryk.ca>
2022-04-14 23:33:51 -0700

    added the game's function

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 
+    let hearts = 10;
     let lettersClicked = "";
     let correctGuessed = ['_ ','_ ','_ ','_ ','_ ','_ '];
     const keyword = "action";
     const letters = document.querySelectorAll(".keyboard-letters");
+    const failureSound = (new Audio("staticAssets/sounds/failure.wav",volume = 0.1));
+
 
     letters.forEach(node =>{
         node.addEventListener('click', event=>{
@@ -22,7 +25,6 @@ $(document).ready(function(){
             //when a letter is clicked it can not be clicked again as it gets disabled 
             $(event.target).attr("disabled","true");
             
-
             //checking to see if the clicked letter is in the phrase
             if (keyword.includes(clickedLetter)) {
 
@@ -43,9 +45,19 @@ $(document).ready(function(){
                 $('h1').html(`${correctGuessed.join('')}`)
                 // $("#guess-bar").last().prepend(`<h1 class="guess-bar-line">${correctGuessed}</h1>`);
                 
-                
+            } else {
+
+                hearts -= 1
+                console.log(hearts);
+
+                if (hearts <= 0) {
+                    failureSound.play();
+                    alert("Game over try again, click ok to refresh the page");
+                    location.reload();
+                }
+                //add more body parts
             }
-            
+
         })
         
     })    
